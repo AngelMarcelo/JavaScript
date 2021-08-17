a13ee85fd6fc1ab1fe028c4bc5b517b2db28e4d4
AngelMarcelo <ammarce@gmail.com>
2021-08-17 18:34:24 -0300

    se agrega pagina de ejercicios

---
+++ JS/holaMundo.js
@@ -0,0 +1,31 @@
+// esto es un comentario simple o de una sola linea
+
+/*esto es un comentario 
+de multiples lineas*/
+
+//mostrar un mensaje por consola
+console.log("hola comision 17");
+
+// mostrr un mensaje en la pagina web
+document.write("Este es mi primer mensaje")
+
+//mostar una ventana emergente
+//alert("esto es una alerta") 
+
+//crear o declara variables var - let - cosnt
+
+let palabraBuscada = "hola";
+
+document.write("<br>variable palabra buscada: "+ palabraBuscada)
+
+// modificar el valor de una variable
+palabraBuscada = "pagina de blog";
+document.write("<br>variable palabra buscada: "+ palabraBuscada)
+
+const comision = 17;
+document.write("<br>variable palabra buscada: "+ comision);
+
+let luz = false;
+
+let suma = 20;
+console.log(++suma);
