ff5cca81ec82378ad16e19bd5abdd15b0d69d46e
AngelMarcelo <ammarce@gmail.com>
2021-08-19 17:02:33 -0300

    solucion ejercicio 5

---
+++ JS/ejercicios1-5.js
@@ -0,0 +1,8 @@
+// paseInt ("numero") = se transforma a formato entero
+let numero1 = parseInt(prompt("ingrese un primer numero")) ;
+let numero2 = parseInt (prompt("ingrese un seguundo numero"));
+
+document.write(numero1+numero2)
+
+console.log(numero1);
+console.log(numero2);
